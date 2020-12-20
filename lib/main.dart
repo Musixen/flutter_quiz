848c592c2060a4a482591f5839b2cc9e302455d9
Musixen <altenburgern@bzz.ch>
2020-12-20 17:22:05 +0100

    Designing

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
-  Widget build(BuildContext context){
-    return MaterialApp(home: Text("Hello!"),);
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Text("This is my default text!"),
+      ),
+    );
   }
 }
